3058e2f17a28764c9c4b0cb07fec93480c7ba2a2
Andrey Pugachenko <andrqxa@gmail.com>
2016-04-16 02:01:32 +0300

    Creating Application

---
+++ puga4enko/PocketSecretary/app/src/main/java/com/andrejjj/pocketsecretary/DayEvents.java
@@ -0,0 +1,13 @@
+package com.andrejjj.pocketsecretary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DayEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.day_events);
+    }
+}
