dc752e5561097c20bd45614437e4f661274e89f9
Vitaliy Gerdt <vitaliy.gerdt@flant.ru>
2016-04-22 15:10:50 +0300

    dict handler

--- gerdtv/DD/app/src/main/java/betaru/dd/DictActivity.java
+++ gerdtv/DD/app/src/main/java/betaru/dd/DictActivity.java
@@ -11,13 +11,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class DictActivity extends AppCompatActivity implements View.OnClickListener {
 
     final String LOG_TAG = "myLogs";
 
     Button btnAdd, btnRead, btnClear;
-    EditText etword, etTrans;
+    EditText etWord, etTrans;
+    TextView txvGerResult, txvRusResult;
 
     DBHelper dbHelper;
 
@@ -30,14 +32,12 @@ public class DictActivity extends AppCompatActivity implements View.OnClickListe
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(this);
 
-        btnRead = (Button) findViewById(R.id.btnRead);
-        btnRead.setOnClickListener(this);
 
-        btnClear = (Button) findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(this);
-
-        etword = (EditText) findViewById(R.id.etWord);
+        etWord = (EditText) findViewById(R.id.etWord);
         etTrans = (EditText) findViewById(R.id.etTrans);
+
+        txvGerResult = (TextView) findViewById(R.id.txv_ger_result);
+        txvRusResult = (TextView) findViewById(R.id.txv_rus_result);
 
         // создаем объект для создания и управления версиями БД
         dbHelper = new DBHelper(this);
@@ -47,11 +47,19 @@ public class DictActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        // переменные для query
+        String[] columns = null;
+        String selection = null;
+        String selectionArgs = null;
+
         // создаем объект для данных
         ContentValues cv = new ContentValues();
 
+        // курсор
+        Cursor c = null;
+
         // получаем данные из полей ввода
-        String word = etword.getText().toString();
+        String word = etWord.getText().toString();
         String trans = etTrans.getText().toString();
 
         // подключаемся к БД
@@ -68,41 +76,72 @@ public class DictActivity extends AppCompatActivity implements View.OnClickListe
                 // вставляем запись и получаем ее ID
                 long rowID = db.insert("mydict", null, cv);
                 Log.d(LOG_TAG, "row inserted, ID = " + rowID);
-                break;
-            case R.id.btnRead:
-                Log.d(LOG_TAG, "--- Rows in mydict: ---");
-                // делаем запрос всех данных из таблицы mydict, получаем Cursor
-                Cursor c = db.query("mydict", null, null, null, null, null, null);
 
-                // ставим позицию курсора на первую строку выборки
-                // если в выборке нет строк, вернется false
-                if (c.moveToFirst()) {
+                selection = "id = ?";
+                selectionArgs = String.valueOf(rowID);
+                c = db.query("mydict", null, selection, new String[]{selectionArgs}, null, null,
+                        null);
 
-                    // определяем номера столбцов по имени в выборке
-                    int idColIndex = c.getColumnIndex("id");
-                    int wordColIndex = c.getColumnIndex("word");
-                    int TransColIndex = c.getColumnIndex("trans");
-
-                    do {
-                        // получаем значения по номерам столбцов и пишем все в лог
-                        Log.d(LOG_TAG,
-                                "ID = " + c.getInt(idColIndex) +
-                                        ", word = " + c.getString(wordColIndex) +
-                                        ", trans = " + c.getString(TransColIndex));
-                        // переход на следующую строку
-                        // а если следующей нет (текущая - последняя), то false - выходим из цикла
-                    } while (c.moveToNext());
-                } else
-                    Log.d(LOG_TAG, "0 rows");
-                c.close();
                 break;
-            case R.id.btnClear:
-                Log.d(LOG_TAG, "--- Clear mydict: ---");
-                // удаляем все записи
-                int clearCount = db.delete("mydict", null, null);
-                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
-                break;
+//            case R.id.btnRead:
+//                Log.d(LOG_TAG, "--- Rows in mydict: ---");
+//                // делаем запрос всех данных из таблицы mydict, получаем Cursor
+//                Cursor c = db.query("mydict", null, null, null, null, null, null);
+//
+//                // ставим позицию курсора на первую строку выборки
+//                // если в выборке нет строк, вернется false
+//                if (c.moveToFirst()) {
+//
+//                    // определяем номера столбцов по имени в выборке
+//                    int idColIndex = c.getColumnIndex("id");
+//                    int wordColIndex = c.getColumnIndex("word");
+//                    int TransColIndex = c.getColumnIndex("trans");
+//
+//                    do {
+//                        // получаем значения по номерам столбцов и пишем все в лог
+//                        Log.d(LOG_TAG,
+//                                "ID = " + c.getInt(idColIndex) +
+//                                        ", word = " + c.getString(wordColIndex) +
+//                                        ", trans = " + c.getString(TransColIndex));
+//                        // переход на следующую строку
+//                        // а если следующей нет (текущая - последняя), то false - выходим из цикла
+//                    } while (c.moveToNext());
+//                } else
+//                    Log.d(LOG_TAG, "0 rows");
+//                c.close();
+//                break;
+//            case R.id.btnClear:
+//                Log.d(LOG_TAG, "--- Clear mydict: ---");
+//                // удаляем все записи
+//                int clearCount = db.delete("mydict", null, null);
+//                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+//                break;
         }
+
+        if (c != null) {
+            if (c.moveToFirst()) {
+                String str;
+                // GER
+                txvGerResult.setEnabled(true);
+                txvGerResult.setText(c.getString(c.getColumnIndex("word")));
+
+                // RUS
+                txvRusResult.setEnabled(true);
+                txvRusResult.setText(c.getString(c.getColumnIndex("trans")));
+
+                //Clear Edit + set focus
+                etWord.setText("");
+                etWord.requestFocus();
+
+                etTrans.setText("");
+
+                Log.d(LOG_TAG, c.getString(c.getColumnIndex("word")));
+            }
+            c.close();
+        } else
+            Log.d(LOG_TAG, "Cursor is null");
+
+
         // закрываем подключение к БД
         dbHelper.close();
     }
