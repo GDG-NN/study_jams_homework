5ed5d4553cd8927389bb5603c235764e4eef528f
Andrey Pugachenko <andrqxa@gmail.com>
2016-04-27 03:33:59 +0300

    Added ActionBar menu

--- puga4enko/PocketSecretary/app/src/main/java/com/andrejjj/pocketsecretary/activities/AddEvent.java
+++ puga4enko/PocketSecretary/app/src/main/java/com/andrejjj/pocketsecretary/activities/AddEvent.java
@@ -1,7 +1,11 @@
 package com.andrejjj.pocketsecretary.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.andrejjj.pocketsecretary.R;
 
@@ -19,5 +23,30 @@ public class AddEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions, menu);
+        return (super.onCreateOptionsMenu(menu));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mniAnalytics:
+                Intent intentAnalytics = new Intent(this, com.andrejjj.pocketsecretary.activities.Analytics.class);
+                startActivity(intentAnalytics);
+                return (true);
+            case R.id.mniOptions:
+                Intent intentOptions = new Intent(this, com.andrejjj.pocketsecretary.activities.Options.class);
+                startActivity(intentOptions);
+                return (true);
+            case R.id.mniAbout:
+                Toast.makeText(this, R.string.about_toast, Toast.LENGTH_LONG)
+                        .show();
+                return (true);
+        }
+        return (super.onOptionsItemSelected(item));
     }
 }
